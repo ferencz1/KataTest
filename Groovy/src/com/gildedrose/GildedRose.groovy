8be79c3dfda9c9ecb1347a918fc6bb6ea130788f
Fran Pelc <ferencz.fran@gmail.com>
2019-03-16 21:07:27 +0100

    Refactoring GildedRose
    -added conditions for "conjured" items,
    -added condition for twice decreasing rate,
    -cleaned code,
    -added test cases in TexttestFixture.groovy

--- Groovy/src/com/gildedrose/GildedRose.groovy
+++ Groovy/src/com/gildedrose/GildedRose.groovy
@@ -7,94 +7,68 @@ class GildedRose {
         this.items = items
     }
 
-    /*void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
-                }
-            }
-        }
-    }*/
     void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             String name = items[i].name.toLowerCase()
-            if (items[i].quality > 0){
-                if (!name.equals("aged brie") && !name.contains("backstage")
-                        && !name.contains("conjured") && !name.contains("sulfuras")){
+            int min_qual = 0
+            int max_qual = 50
+            if (!name.equals("aged brie") && !name.contains("backstage passes") && !name.contains("sulfuras")) {
+                if (items[i].quality > min_qual) {
+                    items[i].quality = items[i].quality - 1
+
+                    if(name.contains("conjured") && items[i].quality > min_qual) {
+                        items[i].quality = items[i].quality - 1
+                    }
+                }
+            }
+            else {
+                if (items[i].quality < max_qual) {
+                    /**
+                     * increase quality for "backstage" and "others"
+                    */
+                    items[i].quality = items[i].quality + 1
+
+                    if (name.contains("backstage passes")) {
+                        /**
+                         * increase "backstage" for tickets in 10 days
+                         */
+                        if (items[i].sellIn < 11) {
+                            if (items[i].quality < max_qual) {
+                                items[i].quality = items[i].quality + 1
+                            }
+                        }
+                        /**
+                         * increase "backstage" for tickets in 5 days
+                         */
+                        if (items[i].sellIn < 6) {
+                            if (items[i].quality < max_qual) {
+                                items[i].quality = items[i].quality + 1
+                            }
+                        }
+                    }
+                }
+            }
+            if (items[i].sellIn < 0) {
+                /**
+                 * if it's "conjured", decrease by one
+                 */
+                if (name.contains("conjured") && items[i].quality > 0) {
                     items[i].quality = items[i].quality - 1
                 }
-                else if(name.contains("conjured")){
-                    items[i].quality = items[i].quality - 2
+                /**
+                 * if it's "conjured" or "other", decrease by one
+                 */
+                if (!name.equals("aged brie") && !name.contains("backstage passes") && !name.contains("sulfuras")
+                        && items[i].quality > 0) {
+                    items[i].quality = items[i].quality - 1
                 }
             }
-            else if(items[i].quality < 50){
-                if(name.equals("aged brie")){
-                    items[i].quality = items[i].quality + 1
-                }
-                else if(name.contains("backstage")){
-                    if(items[i].sellIn > 10){
-                        items[i].quality = items[i].quality + 1
-                    }
-                    else if(items[i].sellIn <= 10 && items[i].sellIn > 5){
-                        items[i].quality = items[i].quality + 2
-                    }
-                    else if(items[i].sellIn <= 5 && items[i].sellIn >= 0){
-                        items[i].quality = items[i].quality + 3
-                    }
-                    else
-                    {
-                        items[i].quality = 0
-                    }
-                }
-            }
+            /**
+             * decrease sellIn for all, except "sulfuras"
+             */
             if (!name.contains("sulfuras")) {
                 items[i].sellIn = items[i].sellIn - 1
             }
-
         }
     }
 }
--- Groovy/src/com/gildedrose/TexttestFixture.groovy
+++ Groovy/src/com/gildedrose/TexttestFixture.groovy
@@ -3,20 +3,22 @@ package com.gildedrose
 println("OMGHAI!")
 
 Item[] items = [
-        new Item("+5 Dexterity Vest", 10, 20),
+        new Item("+5 Dexterity Vest", 2, 55),
         new Item("Aged Brie", 2, 0),
+        new Item("Aged Brie", 2, 48),
         new Item("Elixir of the Mongoose", 5, 7),
         new Item("Sulfuras, Hand of Ragnaros", 0, 80),
         new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-        new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-        new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-        new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 13, 20),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 10, 47),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 5, 46),
         // this conjured item does not work properly yet
-        new Item("Conjured Mana Cake", 3, 6)] as Item[]
+        new Item("Conjured Brie", 2, 10),
+        new Item("Conjured Mana Cake", 3, 3)] as Item[]
 
 GildedRose app = new GildedRose(items)
 
-int days = 3
+int days = 5
 if (args.length > 0) {
     days = Integer.parseInt(args[0]) + 1
 }
